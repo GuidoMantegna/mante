7a6cf0fa984f477e255ed06757843a5530799346
Guido Mantegna <mantegnaguido@gmail.com>
2026-08-27 11:23:12 -0300

    Update Projects Look & Feel

--- components/projects-section.tsx
+++ components/projects-section.tsx
@@ -2,21 +2,47 @@ import Image from "next/image";
 
 export function ProjectsSection() {
   return (
-    <main className="h-svh w-full flex">
-      <section className="section-left relative flex flex-col justify-end w-[35%] px-4 py-12 border-r-1">
-          <div className="border-b mb-4" />
-          <div className="text-xl">
-            <p>COCINAS</p>
-            <p>PLACARES</p>
-            <p>VESTIDORES</p>
+    <main className="section-main">
+      <section className="section-left">
+        <div className="w-full h-full flex flex-col justify-around">
+          {/* SECTION TITLE */}
+          <div>
+            <Image
+              alt="Logo Manté"
+              src="/logo-accent.svg"
+              width={30}
+              height={30}
+            />
+            <h1 className="section-title">Nuestros</h1>
+            <h1 className="section-title text-cancel">Proyectos</h1>
           </div>
+          {/* PROJECTS SELECTOR */}
+          <div className="flex flex-col items-start gap-2">
+            {/* DIVIDER */}
+            <div className="text-xs grid w-full">
+              <Image
+                alt="Dibujo Cocinas"
+                src="/images/cocinas-draw.png"
+                className="my-0 mx-auto"
+                width={200}
+                height={200}
+              />
+              <div className="border-b border-cancel" />
+            </div>
+            <button className="text-2xl font-bold underline text-accent">
+              COCINAS
+            </button>
+            <button className="text-2xl">PLACARES</button>
+            <button className="text-2xl">VESTIDORES</button>
+          </div>
+        </div>
       </section>
-      <section className="section-left relative h-full flex-1 border-r-1">
+      <section className="section-right">
         <Image
           src="/images/splash-3.webp"
           alt="texture"
           fill
-          sizes="100vw"
+          sizes="100%"
           // priority={}
           className="object-cover"
         />
